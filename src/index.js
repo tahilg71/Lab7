2788f174ca86aeb5577f268d1df86143d020cb35
tahilg71 <144259936+tahilg71@users.noreply.github.com>
2024-03-06 16:47:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ class Square extends React.Component {
         };
     }
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+          <Square 
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+          />;
     }
   
     render() {
